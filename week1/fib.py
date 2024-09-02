6cdaab680ffa8bfe6e40549b85d2628346777233
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2024-09-02 05:19:10 +0200

    fib rep

---
+++ week1/fib.py
@@ -0,0 +1,13 @@
+
+def fib(n):
+    result = []
+    a, b = 0, 1
+    while a <= n:
+        result.append(a)
+        a, b = b, (a + b)
+    return result
+
+def main():
+    print(fib(15))
+
+main()
