80ef02d9b9d31ac795292c7d9a0e70fa4125b709
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2024-12-18 10:02:18 +0100

    binary search rep

---
+++ week015/2binary_search.py
@@ -0,0 +1,22 @@
+
+def binary_search(nums, target):
+    low, high = 0, len(nums) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        mid_value = nums[mid]
+        if target == mid_value:
+            return mid
+        elif target < mid_value:
+            high = mid - 1
+        else:
+            mid = low + 1
+    return mid
+
+def main():
+    nums = [0, 2, 3, 6, 9]
+    target = 0
+    index = binary_search(nums, target)
+    print(index)
+
+if __name__ == '__main__':
+    main()
